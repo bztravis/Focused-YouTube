a1cac14d435f3e9c74ed156eeba6340c42fd1a9c
Brian Travis <bztravis@umich.edu>
2022-09-27 13:37:25 -0400

    Initial commit

---
+++ play.js
@@ -0,0 +1,25 @@
+const forceTheater = setInterval(function() {
+    let video = document.querySelector('.html5-video-container')
+    let theaterActive = video.offsetWidth == window.innerWidth
+
+    let tBtn = document.getElementsByClassName('ytp-size-button')
+    button = tBtn[0]
+
+    if (button != null) {
+        if (!theaterActive) {        
+            button.click()
+        }
+
+        button.remove()
+
+        if (theaterActive) {
+            clearInterval(forceTheater)
+        }
+    }
+
+}, 1000)
+
+
+/* const setSearchFocus = setTimeout(() => {
+    document.querySelector('input').focus()
+}, 0) */
