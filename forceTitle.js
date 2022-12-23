37afc572ab3cfc41382cd7da4913db31e4a92627
Brian Travis <bztravis88@gmail.com>
2022-12-23 00:32:17 -0500

    Format Enhanced Home correctly

--- forceTitle.js
+++ forceTitle.js
@@ -1,14 +1,25 @@
-const originalTitle = document.title
+let attempts = 0
 
-let firstUsefulLetter = 0
+const forceTitleInterval = setInterval(() => {
+  // console.log('attempting')
+  const originalTitle = document.title
 
-if (originalTitle[0] === '(') {
-  for (; firstUsefulLetter < originalTitle.length; firstUsefulLetter++) {
-    if (originalTitle[firstUsefulLetter] === ')') {
-      firstUsefulLetter += 2
-      break
+  let firstUsefulLetter = 0
+
+  if (originalTitle[0] === '(') {
+    // alert('working')
+    for (; firstUsefulLetter < originalTitle.length; firstUsefulLetter++) {
+      if (originalTitle[firstUsefulLetter] === ')') {
+        firstUsefulLetter += 2
+        break
+      }
     }
   }
-}
 
-document.title = originalTitle.slice(firstUsefulLetter)
+  document.title = originalTitle.slice(firstUsefulLetter)
+  if (document.title[0] !== '(' || attempts > 30) {  // 30 sec of attempts
+    clearInterval(forceTitleInterval)
+  }
+  
+  attempts++
+}, 1000)
--- play.js
+++ play.js
@@ -22,33 +22,31 @@ const forceTheater = setInterval(function () {
       }
     }
   }
-
-  //   chrome.runtime.sendMessage({ name: 'message' }, (response) => {
-  //     document.querySelector('body').innerHTML = response
-  //   })
 }, 1000)
 
 let isChecked = false
 
-chrome.storage.local.get('isChecked', (result) => {
-  isChecked = result.isChecked
-  // alert(isChecked)
-  // Use the value of isChecked in your content script
+// chrome.storage.local.get('isChecked', (result) => {
+//   isChecked = result.isChecked
+//   // alert(isChecked)
+//   // Use the value of isChecked in your content script
 
-  if (isChecked) {
-    const checkChecked = setInterval(() => {
-      console.log('hi there')
-      if (
-        document.querySelector(
-          'ytd-browse[page-subtype~=channels] ytd-two-column-browse-results-renderer.ytd-browse'
-        )
-      )
-        document.querySelector(
-          'ytd-browse[page-subtype~=channels] ytd-two-column-browse-results-renderer.ytd-browse'
-        ).style.display = 'none'
-      clearInterval(checkChecked)
-    }, 100)
-  }
-})
+//   if (isChecked) {
+//     const checkChecked = setInterval(() => {
+//       console.log('hi there')
+//       if (
+//         document.querySelector(
+//           'ytd-browse[page-subtype~=channels] ytd-two-column-browse-results-renderer.ytd-browse'
+//         )
+//       )
+//         document.querySelector(
+//           'ytd-browse[page-subtype~=channels] ytd-two-column-browse-results-renderer.ytd-browse'
+//         ).style.display = 'none'
+//       clearInterval(checkChecked)
+//     }, 100)
+//   }
+// })
 
-// alert("hi" + isChecked)
+
+
+document.querySelector('#logo-icon').addEventListener('click', () => {window.open("https://www.youtube.com","_self")})
